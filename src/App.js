e30807b7a733ba2e03b2422eb4995bbaa9336c99
Achen7068 <you@example.com>
2020-10-06 21:09:58 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from "./Component/Header";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Router>
+
+        </Router>
+
     </div>
   );
 }
