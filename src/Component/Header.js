dee759a0bc0b78799ee98ca49eed7950ea7bc09a
Alex Chen <you@example.com>
2020-10-06 21:04:20 -0400

    Fixed Header Issue

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ForumIcon from '@material-ui/icons/Forum';
 import PersonIcon from '@material-ui/icons/Person';
-import styles from "./Header.modules.css"
+import IconButton from "@material-ui/core/IconButton";
+import styles from './Header.module.css';
 
 function Header (){
         return (
             <div className={styles.header}>
-                <ForumIcon className={styles.headerIcon} fontSize={"large"}/>
-                    <img className={styles.headerLogo} src={"https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"} alt={"tinderLogo"}/>
-                <PersonIcon className={styles.headerIcon} fontSize={"large"}/>
+                <IconButton>
+                    <ForumIcon className={styles.headerIcon} fontSize={"large"}/>
+                </IconButton>
+                <img className={styles.headerLogo} src={"https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"} alt={"tinderLogo"}/>
+                <IconButton>
+                    <PersonIcon className={styles.headerIcon} fontSize={"large"}/>
+                </IconButton>
+
             </div>
         );
-
 }
 
 export default Header;
